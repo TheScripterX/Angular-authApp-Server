df0c7a312cbc3d7606840190f7782864aa16fc49
TheScripterX <hamdounwassim94@gmail.com>
2021-08-25 23:16:51 +0000

    V 0.1.7 First POST request with UserModel

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,40 @@
 const { response, request } = require("express");
+const User = require("../models/User");
 
-const createUser = (req = request, res = response) => {
+const createUser = async (req = request, res = response) => {
   const { name, email, password } = req.body;
+  console.log(req.body);
 
-  return res.json({
-    ok: true,
-    msg: "Create new user /new",
-  });
+  try {
+    // Check Email if already exist
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "This email already exists!",
+      });
+    }
+
+    // Create User with User Model
+    user = new User(req.body);
+
+    // Save it to Database
+    await user.save();
+
+    // Generate success response
+    return res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please contact Admin",
+    });
+  }
 };
 
 const loginUser = (req = request, res = response) => {
